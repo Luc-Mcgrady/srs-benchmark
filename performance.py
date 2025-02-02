84c76cafb2a5b1a978b5d476e53e7beebdb2bee4
Luc Mcgrady <lucmcgrady@gmail.com>
2025-02-02 22:43:11 +0000

    performance-cuda

--- performance.py
+++ performance.py
@@ -1,4 +1,5 @@
 # You can pass arguments to this script as if it were script.py
+from multiprocessing.dummy import freeze_support
 from statistics import mean
 import script
 import timeit
@@ -10,6 +11,7 @@ import os
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import tracemalloc
+import multiprocessing
 
 # Config
 B_TIME = bool(
@@ -19,9 +21,9 @@ N = int(os.environ.get("N", 50))  # Number of users to sample
 MEMORY = bool(os.environ.get("MEM", False)) # Significantly impacts run speed
 
 # Graph Display Info
-A_NAME = "A"
-B_NAME = "B"
-TITLE = "Generic"
+A_NAME = "CPU"
+B_NAME = "CUDA"
+TITLE = "seqlen64 CUDA (infinite batch size) vs CPU"
 
 # Don't change
 USER_COUNT = 10000
@@ -68,12 +70,16 @@ def process_wrapper(uid: int):
 
 
 def process_wrapper_a(uid: int):
-    torch.set_num_threads(2)
+    script.DEVICE = "cpu"
+    script.batch_size = 512
+    script.max_seq_len = 64
     return process_wrapper(uid)
 
 
 def process_wrapper_b(uid: int):
-    torch.set_num_threads(3)  # Num threads example
+    script.DEVICE = "cuda"
+    script.max_seq_len = 64
+    script.batch_size = 1_000_000_000_000_000
     return process_wrapper(uid)
 
 
@@ -82,112 +88,114 @@ def performance_process(uid: int, i: int, wrapper, name):
     loss = result["metrics"]["LogLoss"]
     return uid, i, time, memory, loss, name
 
+if __name__ == "__main__":
+    freeze_support()
+    multiprocessing.set_start_method("spawn", True)
+    with ProcessPoolExecutor(script.PROCESSES) as executor:
 
-with ProcessPoolExecutor(script.PROCESSES) as executor:
+        future_args = [
+            (
+                [
+                    (performance_process, sizes[user_index][0], i, process_wrapper_a, A_NAME),
+                    (performance_process, sizes[user_index][0], i, process_wrapper_b, B_NAME),
+                ]
+                if B_TIME
+                else [
+                    (performance_process, sizes[user_index][0], i, process_wrapper_a, A_NAME),
+                ]
+            )
+            for i, user_index in enumerate(indexes)
+        ]
 
-    future_args = [
-        (
-            [
-                (performance_process, sizes[user_index][0], i, process_wrapper_a, A_NAME),
-                (performance_process, sizes[user_index][0], i, process_wrapper_b, B_NAME),
-            ]
-            if B_TIME
-            else [
-                (performance_process, sizes[user_index][0], i, process_wrapper_a, A_NAME),
-            ]
+        futures = [executor.submit(*args) for argss in future_args for args in argss]
+
+        for future in (
+            progress := tqdm(as_completed(futures), total=len(futures), smoothing=0.03)
+        ):
+            uid, i, time, memory, loss, name = future.result()
+            progress.set_description(f"{uid=}, rows={sizes[uid][1]}, {name}={time:.2f}s")
+            if name == A_NAME:
+                a_times[i] = time
+                a_losses[i] = loss
+                a_memory[i] = memory
+            else:
+                b_times[i] = time
+                b_losses[i] = loss
+                b_memory[i] = memory
+
+
+    total_a_time = sum(a_times)
+    total_b_time = sum(b_times)
+
+
+    def estimate_time(secs: int):
+        return (secs * USER_COUNT) / (N * script.PROCESSES)
+
+
+    print(f"total a_time for {N} users={total_a_time:.2f}s")
+    if B_TIME:
+        print(f"total b_time for {N} users={total_b_time:.2f}s")
+    print("")
+
+    print(
+        f"Estimated total a_time ({script.PROCESSES} process)={estimate_time(total_a_time):.2f}s"
+    )
+    print(
+        f"Estimated total a_time ({script.PROCESSES} process)={estimate_time(total_a_time) / 60 * 60:.2f}h"
+    )
+    if B_TIME:
+        print(
+            f"Estimated total b_time for {USER_COUNT} users (one process)={estimate_time(total_b_time) * USER_COUNT / N:.2f}s"
         )
-        for i, user_index in enumerate(indexes)
-    ]
+        print(
+            f"Estimated total b_time for {USER_COUNT} users (one process)={estimate_time(total_b_time) / (60 * 60):.2f}h"
+        )
 
-    futures = [executor.submit(*args) for argss in future_args for args in argss]
+    print("")
+    print(f"{mean(a_losses)=:.5f}")
+    if B_TIME:
+        print(f"{mean(b_losses)=:.5f}")
 
-    for future in (
-        progress := tqdm(as_completed(futures), total=len(futures), smoothing=0.03)
-    ):
-        uid, i, time, memory, loss, name = future.result()
-        progress.set_description(f"{uid=}, rows={sizes[uid][1]}, {name}={time:.2f}s")
-        if name == A_NAME:
-            a_times[i] = time
-            a_losses[i] = loss
-            a_memory[i] = memory
-        else:
-            b_times[i] = time
-            b_losses[i] = loss
-            b_memory[i] = memory
+    GRAPHS = 2 if not MEMORY else 3
 
+    plt.suptitle(TITLE)
 
-total_a_time = sum(a_times)
-total_b_time = sum(b_times)
-
-
-def estimate_time(secs: int):
-    return (secs * USER_COUNT) / (N * script.PROCESSES)
-
-
-print(f"total a_time for {N} users={total_a_time:.2f}s")
-if B_TIME:
-    print(f"total b_time for {N} users={total_b_time:.2f}s")
-print("")
-
-print(
-    f"Estimated total a_time ({script.PROCESSES} process)={estimate_time(total_a_time):.2f}s"
-)
-print(
-    f"Estimated total a_time ({script.PROCESSES} process)={estimate_time(total_a_time) / 60 * 60:.2f}h"
-)
-if B_TIME:
-    print(
-        f"Estimated total b_time for {USER_COUNT} users (one process)={estimate_time(total_b_time) * USER_COUNT / N:.2f}s"
-    )
-    print(
-        f"Estimated total b_time for {USER_COUNT} users (one process)={estimate_time(total_b_time) / (60 * 60):.2f}h"
-    )
-
-print("")
-print(f"{mean(a_losses)=:.5f}")
-if B_TIME:
-    print(f"{mean(b_losses)=:.5f}")
-
-GRAPHS = 2 if not MEMORY else 3
-
-plt.suptitle(TITLE)
-
-plt.subplot(1, GRAPHS, 1)
-plt.xlabel(f"Revlogs (total={sum(row_counts)})")
-plt.ylabel(f"Seconds")
-plt.plot(
-    row_counts,
-    a_times,
-    label=f"{A_NAME} {N} in {sum(a_times):.2f}s, estimated={estimate_time(total_a_time) / (60 * 60):.2f}h",
-)
-if B_TIME:
+    plt.subplot(1, GRAPHS, 1)
+    plt.xlabel(f"Revlogs (total={sum(row_counts)})")
+    plt.ylabel(f"Seconds")
     plt.plot(
         row_counts,
-        b_times,
-        label=f"{B_NAME} {N} in {sum(b_times):.2f}s, estimated={estimate_time(total_b_time) / (60 * 60):.2f}h",
+        a_times,
+        label=f"{A_NAME} {N} in {sum(a_times):.2f}s, estimated={estimate_time(total_a_time) / (60 * 60):.2f}h",
     )
-plt.title(f"Time Spent")
-plt.legend()
-
-if MEMORY:
-    plt.subplot(1, GRAPHS, 2)
-    plt.xlabel(f"Revlogs")
-
-    plt.ylabel(f"Bytes")
-    plt.plot(row_counts, a_memory, label=f"{A_NAME} avg={mean(a_memory):.5f}")
     if B_TIME:
-        plt.plot(row_counts, b_memory, label=f"{B_NAME} avg={mean(b_memory):.5f}")
-    plt.title(f"Memory")
+        plt.plot(
+            row_counts,
+            b_times,
+            label=f"{B_NAME} {N} in {sum(b_times):.2f}s, estimated={estimate_time(total_b_time) / (60 * 60):.2f}h",
+        )
+    plt.title(f"Time Spent")
     plt.legend()
 
-plt.subplot(1, GRAPHS, GRAPHS)
-plt.xlabel(f"Revlogs")
+    if MEMORY:
+        plt.subplot(1, GRAPHS, 2)
+        plt.xlabel(f"Revlogs")
 
-plt.ylabel(f"Log Loss")
-plt.plot(row_counts, a_losses, label=f"{A_NAME} avg={mean(a_losses):.5f}")
-if B_TIME:
-    plt.plot(row_counts, b_losses, label=f"{B_NAME} avg={mean(b_losses):.5f}")
-plt.title(f"Loss")
-plt.legend()
+        plt.ylabel(f"Bytes")
+        plt.plot(row_counts, a_memory, label=f"{A_NAME} avg={mean(a_memory):.5f}")
+        if B_TIME:
+            plt.plot(row_counts, b_memory, label=f"{B_NAME} avg={mean(b_memory):.5f}")
+        plt.title(f"Memory")
+        plt.legend()
 
-plt.show()
+    plt.subplot(1, GRAPHS, GRAPHS)
+    plt.xlabel(f"Revlogs")
+
+    plt.ylabel(f"Log Loss")
+    plt.plot(row_counts, a_losses, label=f"{A_NAME} avg={mean(a_losses):.5f}")
+    if B_TIME:
+        plt.plot(row_counts, b_losses, label=f"{B_NAME} avg={mean(b_losses):.5f}")
+    plt.title(f"Loss")
+    plt.legend()
+
+    plt.show()
